61e3f84c4e4bc2fbdd04d33e383c76d88e202991
romuloboeck <159554566+romuloboeck@users.noreply.github.com>
2024-10-09 11:20:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,55 @@
+criaCartao(
+    'academia',
+    'eexercicio bom para pitoral?',
+    'supino'
+)
+
+criaCartao(
+    'academia',
+    'melhor suplento ja criado?',
+    'creatina'
+)
+
+criaCartao(
+    'academia',
+    'estado onde ramon dino nasceu?',
+    'acre'
+)
+
+criaCartao(
+    'acadenia',
+    'melhor anaboliante?',
+    'trembolona'
+)
+
+criaCartao(
+    'academia' ,
+    'aonde cbum nasceu ?' ,
+    'nascionaliado no canada'
+)
+
+criaCartao(
+    'jogo' ,
+    'jogo mais viciante de todos?' ,
+    'brawl stars'
+)
+criaCartao(
+    'brasil' ,
+    'melhor presidente do brasil' ,
+    'bolsonaro'
+)
+criaCartao(
+    'pedreiro' ,
+    'com quantos paus se fazem uma canoa ?' ,
+    'dependdo o tamanho da arvore '
+)
+ criaCartao(
+    'academia' ,
+    'qual o melhor ciclo de anabolizantes?' ,
+    'testo e deca'
+ )
+ criaCartao(
+    'pessoal' ,
+    'quantos anos lula tem' ,
+    '76 anos e de cadeia 42'
+ )
